ece7baf042c0241e8da99d50ad37865268862551
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-11-22 23:10:36 -0500

    Simpler gen-grid method

--- grid.rkt
+++ grid.rkt
@@ -2,12 +2,8 @@
 
 (require math)
 
-(define (generate-row width)
-  (sample (discrete-dist '(#t #f)) width))
+(define (gen-grid width height)
+  (for/list ([i height])
+    (sample (discrete-dist '(#t #f)) width)))
 
-(define (generate-grid width height)
-  (for/fold ([grid '()])
-            ([i height])
-    (append grid (list (generate-row width)))))
-
-(generate-grid 20 20)
+(pretty-print (gen-grid 10 10))
