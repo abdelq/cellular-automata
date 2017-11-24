93282177747531dd366d1ed9ff45d644dacefc28
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-11-23 23:11:11 -0500

    Neighbors primitive

--- grid.rkt
+++ grid.rkt
@@ -6,4 +6,15 @@
   (for/list ([i height])
     (sample (discrete-dist '(#t #f) `(,alive ,(- 1 alive))) width)))
 
-(pretty-print (gen-grid 10 10 .5))
+(define (neighbors grid x y)
+  (for*/list ([i (in-range (- y 1) (+ y 2))]
+              [j (in-range (- x 1) (+ x 2))]
+              #:unless (or (equal? (cons i j) (cons y x))
+                           (< i 0) (>= i (length grid))
+                           (< j 0) (>= j (length (first grid)))))
+    (list-ref (list-ref grid i) j)))
+
+(let ([grid (gen-grid 10 10 .5)]
+      [x 5] [y 5])
+  (pretty-print grid)
+  (neighbors grid x y))
