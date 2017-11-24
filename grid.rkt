cf08e519d11f848a8fe034afd47bbc4fb065e60b
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-11-24 18:05:25 -0500

    Default to true when neighbors don't exist

--- grid.rkt
+++ grid.rkt
@@ -2,19 +2,28 @@
 
 (require math)
 
-(define (gen-grid width height alive)
+(define (generate width height alive)
   (for/list ([i height])
     (sample (discrete-dist '(#t #f) (list alive (- 1 alive))) width)))
+
+(define (inside? grid x y)
+  (let ([height (length grid)]
+        [width (length (first grid))])
+    (and (>= x 0)
+         (< x width)
+         (>= y 0)
+         (< y height))))
 
 (define (neighbors grid x y)
   (for*/list ([i (in-range (- y 1) (+ y 2))]
               [j (in-range (- x 1) (+ x 2))]
-              #:unless (or (equal? (cons i j) (cons y x))
-                           (< i 0) (>= i (length grid))
-                           (< j 0) (>= j (length (first grid)))))
-    (list-ref (list-ref grid i) j)))
+              #:unless (equal? (cons i j) (cons y x)))
+    (if (inside? grid j i)
+      (list-ref (list-ref grid i) j)
+      #t)))
 
-(let ([grid (gen-grid 10 10 .5)]
-      [x 5] [y 5])
+(let ([grid (generate 10 10 .5)]
+      [x 1]
+      [y 1])
   (pretty-print grid)
   (neighbors grid x y))
