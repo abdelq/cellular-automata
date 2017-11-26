1119bce41ef0b5c132c8311e6699ffd057fc42d5
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-11-25 19:46:57 -0500

    Generating PNG of dungeon

--- grid.rkt
+++ grid.rkt
@@ -1,28 +1,25 @@
 #lang racket
 
-(require math)
-
-(define (generate width height alive)
-  (for/list ([i height])
-    (sample (discrete-dist '(#t #f) (list alive (- 1 alive))) width)))
+(require math
+         racket/draw)
 
 (define (inside? grid x y)
   (let ([height (length grid)]
         [width (length (first grid))])
-    (and (>= x 0)
-         (< x width)
-         (>= y 0)
-         (< y height))))
+    (and (>= y 0)
+         (< y height)
+         (>= x 0)
+         (< x width))))
 
 (define (neighbors grid x y)
-  (for*/list ([i (in-range (- y 1) (+ y 2))]
-              [j (in-range (- x 1) (+ x 2))]
-              #:unless (equal? (cons i j) (cons y x)))
-    (if (inside? grid j i)
-      (list-ref (list-ref grid i) j)
+  (for*/list ([i (in-range (- x 1) (+ x 2))]
+              [j (in-range (- y 1) (+ y 2))]
+              #:unless (equal? (cons i j) (cons x y)))
+    (if (inside? grid i j)
+      (list-ref (list-ref grid j) i)
       #t)))
 
-(define (next grid x y birth survival)
+(define (iter grid x y birth survival)
   (let ([alive? (list-ref (list-ref grid y) x)]
         [alive-neighbors (count identity (neighbors grid x y))])
     (cond
@@ -35,12 +32,25 @@
         [width (length (first grid))])
     (for/list ([y height])
       (for/list ([x width])
-        (next grid x y birth survival)))))
+        (iter grid x y birth survival)))))
+
+(define (generate width height alive)
+  (for/list ([i height])
+    (sample (discrete-dist '(#t #f) (list alive (- 1 alive))) width)))
 
 (define (automata width height alive birth survival iterations)
   (let ([grid (generate width height alive)])
     (for ([i iterations])
-      (set grid (iterate grid birth survival)))
+      (set! grid (iterate grid birth survival)))
     grid))
 
-(pretty-print (automata 20 20 0.45 5 4 4))
+(let* ([dungeon (automata 100 100 .45 5 4 12)]
+       [grid (make-bitmap (length (first dungeon)) (length dungeon))]
+       [dc (new bitmap-dc% [bitmap grid])])
+  (send dc set-pen "black" 0 'solid)
+  (send dc set-brush "black" 'solid)
+  (for* ([y (length dungeon)]
+         [x (length (first dungeon))])
+      (when (list-ref (list-ref dungeon y) x)
+        (send dc draw-rectangle x y 1 1)))
+  (send grid save-file "dungeon.png" 'png))
